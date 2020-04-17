e4fb208797ecc99bd3168f943aef7072d31a1fdc
lonkecxd <feelingcxd@126.com>
2020-04-17 19:34:25 +0800

    add neo4j func

--- server.js
+++ server.js
@@ -11,20 +11,13 @@ const port = process.env.PORT || 3000;
 // Jsonwebtoken
 const jwt = require('jsonwebtoken');
 // Connect Neo4j in heroku
-var neo4j = require('neo4j-driver');
-var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL || "bolt://hobby-kagcfkdfpfnpgbkemigmppel.dbs.graphenedb.com:24787";
-var graphenedbHttpURL = process.env.GRAPHENEDB_HTTP_URL || "https://hobby-kagcfkdfpfnpgbkemigmppel.dbs.graphenedb.com:24780/db/data/";
-var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER || "cxd";
-var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD || "b.2uCrRnmP9DAr.yOEls3itVFafuWwk";
-var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
-var session = driver.session();
-var superagent = require('superagent');
-// Seraph
-var db = require("seraph")({
-    server: "https://hobby-kagcfkdfpfnpgbkemigmppel.dbs.graphenedb.com:24780/db/data/",
-    user: "cxddddd",
-    pass: "b.2uCrRnmP9DAr.yOEls3itVFafuWwk"
-});
+var neo4j = require('neo4j');
+var graphenedbURL = process.env['GRAPHENEDB_URL'] || "http://localhost:7474/db/data/"
+var neo4jdb = new neo4j.GraphDatabase(graphenedbURL);
+
+// var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER || "cxd";
+// var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD || "b.2uCrRnmP9DAr.yOEls3itVFafuWwk";
+
 
 server.get('/api', (req,res)=>{
     res.json({
@@ -33,31 +26,29 @@ server.get('/api', (req,res)=>{
 });
 server.get('/api/:p1', (req,res)=>{
     var p1 = req.params.p1;
-    session
-        .run("MATCH (p:Person) RETURN p",{p: p1})
-        .then(function(result) {
-
-            result.records.forEach(function(record) {
-
-            });
-            res.json({
-                records:result.records
-            });
-            session.close();
-        })
-        .catch(function(error) {
-            res.json({
-                err: error
-            });
-        });
-    // superagent.post(graphenedbHttpURL+'/cypher').send({
-    //     query: 'MATCH (bacon:Person {name:"Kevin Bacon"})-[*1]-(hollywood)\n' +
-    //         'RETURN DISTINCT bacon,hollywood'
+    // session
+    //     .run("MATCH (p:Person) RETURN p LIMIT 10")
+    //     .then(function(result) {
+    //
+    //         result.records.forEach(function(record) {
+    //             console.log(record);
+    //         });
+    //         res.json({
+    //             records:result.records
+    //         });
+    //         session.close();
+    //     })
+    //     .catch(function(error) {
+    //         res.json({
+    //             err: error
+    //         });
+    //     });
+    // superagent.post("http://localhost:7474/db/data").send({
+    //     query: 'Match (bacon:Person) RETURN bacon'
     // }).end(function (neoRes) {
     //     res.json({text:neoRes.text});
     // })
-    // var cypherQuery = "MATCH (bacon:Person {name:\"Kevin Bacon\"})-[*1]-(hollywood) " +
-    //     "RETURN DISTINCT bacon,hollywood";
+    // var cypherQuery = "MATCH (n:Person) RETURN n";
     // db.query(cypherQuery, function(err, results) {
     //     var result = results[0];
     //     if (err) {
@@ -66,6 +57,21 @@ server.get('/api/:p1', (req,res)=>{
     //         console.log('Node saved to database with id:', result.id);
     //     }
     // });
+    neo4jdb.cypher({
+        query: 'MATCH (n:Person {name: {personName}}) RETURN n',
+        params: {
+            personName: p1
+        }
+    }, function(err, results){
+        var result = results[0];
+        if (err) {
+            console.error('Error saving new node to database:', err);
+        } else {
+            res.json({
+                data: result
+            });
+        }
+    });
 });
 server.post('/api/login',(req,res)=>{
     const user = {
