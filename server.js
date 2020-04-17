b0f38972426897ba0819cab829524dfacdb38573
lonkecxd <feelingcxd@126.com>
2020-04-17 16:28:37 +0800

    add neo4j func

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 // Jsonwebtoken
 const jwt = require('jsonwebtoken');
+// Connect Neo4j in heroku
+var neo4j = require('neo4j-driver').v1;
+var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
+var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
+var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
+var driver = neo4j.driver(graphenedbURL, neo4j.auth.basic(graphenedbUser, graphenedbPass));
+var session = driver.session();
 
 server.get('/api', (req,res)=>{
     res.json({
@@ -16,9 +23,23 @@ server.get('/api', (req,res)=>{
     });
 });
 server.get('/api/ka', (req,res)=>{
-    res.json({
-        message:"Welcome to API!"
-    });
+    session
+        .run("MATCH (bacon:Person {name:\"Kevin Bacon\"})-[*1]-(hollywood)\n" +
+            "RETURN DISTINCT bacon,hollywood")
+        .then(function(result) {
+            result.records.forEach(function(record) {
+                res.json({
+                    record:record
+                });
+            });
+
+            session.close();
+        })
+        .catch(function(error) {
+            res.json({
+                err: error
+            });
+        });
 });
 server.post('/api/login',(req,res)=>{
     const user = {
