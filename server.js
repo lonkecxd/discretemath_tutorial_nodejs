53ed4c46c95f2ff5d7541a885a86c547b7361ab4
lonkecxd <feelingcxd@126.com>
2020-05-05 19:56:25 +0800

    dialogflow query

--- server.js
+++ server.js
@@ -66,12 +66,14 @@ server.get('/api', (req,res)=>{
 server.get('/api/nlp/:q', (req,res)=>{
     var q = req.params.q;
     console.log("NLP Query: "+q);
+    var message = JSON.stringify({
+        "explain": "所谓幂集（Power Set）， 就是原集合中所有的子集（包括全集和空集）构成的集族。",
+        "related": ["可数集","不可数集"]
+    });
+    console.log(message);
     res.json({
         status: 'success',
-        message: JSON.stringify({
-            explain: '所谓幂集（Power Set）， 就是原集合中所有的子集（包括全集和空集）构成的集族。',
-            related: ['可数集','不可数集']
-        })
+        message: message
     });
 });
 
